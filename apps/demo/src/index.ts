3d2e37f37b709b8dd43dbb4bb20ac498e06b71f2
stuartin <13344713+stuartin@users.noreply.github.com>
2025-05-02 17:26:27 -0700

    fix linting

--- apps/demo/src/index.ts
+++ apps/demo/src/index.ts
@@ -1,15 +1,15 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
