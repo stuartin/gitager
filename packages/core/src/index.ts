11ce32ab9e57c5bb2429784fbab06a883c306289
stuartin <13344713+stuartin@users.noreply.github.com>
2025-05-04 00:29:12 -0700

    initial blueprint stubs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,14 @@
 import type { GitOptions } from "@gitager/git-db/types";
 import { createAPI } from "./api";
 import { createServer } from "./lib/orpc";
+import type { BluePrint } from "./api/core/blueprints/blueprints.contract";
 
 export type GitagerOptions = {
     git: GitOptions
     api?: {
         basePath?: `/${string}`
     }
+    blueprints?: BluePrint[]
 };
 
 export type GitagerApp = ReturnType<typeof gitager>
