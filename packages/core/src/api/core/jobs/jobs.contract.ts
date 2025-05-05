c5a0f93cefa8a1c0645fa417f110cc5277d9d369
stuartin <13344713+stuartin@users.noreply.github.com>
2025-05-04 20:18:22 -0700

    update job schema and add types to cursorpagination

--- packages/core/src/api/core/jobs/jobs.contract.ts
+++ packages/core/src/api/core/jobs/jobs.contract.ts
@@ -3,9 +3,15 @@ import { createContract } from '../../../lib/orpc';
 import { NOT_FOUND, UNPROCESSABLE_CONTENT } from '../../../lib/orpc/errors';
 import { CursorPaginationSchema } from '../../../lib/orpc/schemas';
 
+export type Jobs = z.infer<typeof JobsSchema>
 export const JobsSchema = z.object({
     id: z.string().cuid2().describe('The job id'),
     name: z.string().describe('The name of the job'),
+    type: z.enum(['plop']).describe("The type of job"),
+    cron: z.string().default("*/5 * * * *").describe("A cron string for when the job should run"),
+    inputs: z.record(z.string()).optional().describe("Any inputs that the job should receive"),
+    outputs: z.record(z.string()).optional().describe("Any outputs that the job will end with"),
+    status: z.enum(["pending", "in-progress", "completed", "error"]).default("pending").describe("The current status of the job")
 });
 
 const oc = createContract()
--- packages/core/src/lib/orpc/schemas.ts
+++ packages/core/src/lib/orpc/schemas.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+export type CursorPagination = z.infer<typeof CursorPaginationSchema>
 export const CursorPaginationSchema = z.object({
   order: z.enum(['asc', 'desc']).default('asc').describe('Sort responses by id (ascending or descending).'),
   limit: z.number().int().min(1).max(100).default(10).describe('Limit the number of items returned.'),
