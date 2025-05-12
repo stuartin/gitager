a0e10c72ad8b9fb528430a44b3ac79ff510d63e0
stuartin <13344713+stuartin@users.noreply.github.com>
2025-05-12 01:26:39 -0700

    add initial definePlugin

--- packages/core/src/api/index.ts
+++ packages/core/src/api/index.ts
@@ -1,16 +1,19 @@
 import type { GitagerOptions } from '..';
 import { createContract, createRouter } from '../lib/orpc';
+import { t } from '../lib/plugins';
 import { coreContract } from './core/core.contract';
 import { coreRouter } from './core/core.router';
 
 export function createAPI(options: GitagerOptions) {
   const apiContract = createContract().pub.prefix(options.api?.basePath || '/api/v1').router({
     ...coreContract,
+    test: t.contract
   });
 
   const apiRouter = createRouter(apiContract)
     .router({
       ...coreRouter,
+      test: t.router
     });
 
   return apiRouter;
