0bddda26933ba6f61b1dbdecb789d20e98666f03
stuartin <13344713+stuartin@users.noreply.github.com>
2025-05-12 17:34:55 -0700

    update git db exports

--- packages/git-db/src/index.ts
+++ packages/git-db/src/index.ts
@@ -1,6 +1,7 @@
 import { GitDB, type GitDBOptions } from "./db";
+import { type GitOptions } from "./git";
 
-export type { GitDBOptions }
+export type { GitOptions, GitDBOptions, GitDB }
 
 export function createGitDB(options: GitDBOptions) {
     return new GitDB(options)
